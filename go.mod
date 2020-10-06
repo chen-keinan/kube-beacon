d251cd2e22963758f9856c5b98c059702afe85da
chenk keinan <hen.keinan@gmail.com>
2020-10-06 09:40:30 +0300

    Add mod file

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/chen-keinan/beacon
+
+go 1.14
+
+require github.com/mitchellh/cli v1.1.2
--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package beacon
 
-func main() {
+import (
+	"github.com/mitchellh/cli"
+	"log"
+	"os"
+)
 
+func main() {
+	c := cli.NewCLI("beacon", "1.0.0")
+	c.Args = os.Args[1:]
+	c.Commands = map[string]cli.CommandFactory{
+
+	}
+
+	exitStatus, err := c.Run()
+	if err != nil {
+		log.Println(err)
+	}
+
+	os.Exit(exitStatus)
 }
