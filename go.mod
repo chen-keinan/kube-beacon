7b8a0937d4f4439869e0b0da421d729814e598c9
chenk keinan <hen.keinan@gmail.com>
2020-10-06 12:07:50 +0300

    add basic command

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/chen-keinan/beacon
 
 go 1.14
 
-require github.com/mitchellh/cli v1.1.2
+require (
+	github.com/alexellis/go-execute v0.0.0-20200124154445-8697e4e28c5e
+	github.com/mitchellh/cli v1.1.2
+)
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	execute "github.com/alexellis/go-execute/pkg/v1"
 	"github.com/mitchellh/cli"
 	"os"
 )
@@ -29,7 +30,17 @@ func (*Hello) Help() string {
 	return "-t , --test run benchmark tests"
 }
 func (*Hello) Run(args []string) int {
-	fmt.Printf("running benchmark tests, %v", args)
+	ls := execute.ExecTask{
+		Command: "stat -c %a /etc/kubernetes/manifests/kube-apiserver.yaml",
+		Args:    []string{},
+		Shell:   true,
+	}
+	res, err := ls.Execute()
+	if err != nil {
+		fmt.Sprintf("Failed to execute command %s", err.Error())
+	}
+
+	fmt.Printf("stdout: %q, stderr: %q, exit-code: %d\n", res.Stdout, res.Stderr, res.ExitCode)
 	return 0
 }
 func (h *Hello) Synopsis() string {
