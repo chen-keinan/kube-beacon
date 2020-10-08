54af61b8976add909f6dd09880af2c1646186fa2
chenk keinan <hen.keinan@gmail.com>
2020-10-08 10:50:23 +0300

    add args sanitizer

--- cmd/beacon/beacon.go
+++ cmd/beacon/beacon.go
@@ -1,30 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"github.com/chen-keinan/beacon/internal/commands"
-	"github.com/chen-keinan/beacon/internal/startup"
-	"github.com/mitchellh/cli"
-	"os"
-)
+import "github.com/chen-keinan/beacon/internal/startup"
 
 func main() {
-	app := cli.NewCLI("beacon", "1.0.0")
-	// init cli folder and templates
-	startup.StartCli()
-	app.Args = os.Args[1:]
-	app.Commands = map[string]cli.CommandFactory{
-		"audit": func() (cli.Command, error) {
-			return &commands.K8sBenchmark{}, nil
-		},
-		"a": func() (cli.Command, error) {
-			return &commands.K8sBenchmark{}, nil
-		},
-	}
-
-	status, err := app.Run()
-	if err != nil {
-		fmt.Println(err)
-	}
-	os.Exit(status)
+	startup.InitCLI(startup.ArgsSanitizer)
 }
--- internal/startup/cli.go
+++ internal/startup/cli.go
@@ -1,8 +1,13 @@
 package startup
 
 import (
+	"fmt"
+	"github.com/chen-keinan/beacon/internal/commands"
 	"github.com/chen-keinan/beacon/internal/common"
 	"github.com/chen-keinan/beacon/pkg/utils"
+	"github.com/mitchellh/cli"
+	"os"
+	"strings"
 )
 
 //StartCli init beacon cli , folder , templates and etc
@@ -21,3 +26,38 @@ func StartCli() {
 		panic(err)
 	}
 }
+
+//InitCLI initialize beacon cli
+func InitCLI(sa SanitizeArgs) {
+	app := cli.NewCLI("beacon", "1.0.0")
+	// init cli folder and templates
+	StartCli()
+	app.Args = sa(os.Args[1:])
+	app.Commands = map[string]cli.CommandFactory{
+		"audit": func() (cli.Command, error) {
+			return &commands.K8sBenchmark{}, nil
+		},
+		"a": func() (cli.Command, error) {
+			return &commands.K8sBenchmark{}, nil
+		},
+	}
+	status, err := app.Run()
+	if err != nil {
+		fmt.Println(err)
+	}
+	os.Exit(status)
+}
+
+//ArgsSanitizer sanitize CLI arguments
+var ArgsSanitizer SanitizeArgs = func(str []string) []string {
+	args := make([]string, 0)
+	for _, arg := range str {
+		arg = strings.Replace(arg, "--", "", -1)
+		arg = strings.Replace(arg, "-", "", -1)
+		args = append(args, arg)
+	}
+	return args
+}
+
+//SanitizeArgs sanitizer func
+type SanitizeArgs func(str []string) []string
