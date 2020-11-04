6f5b12eff95405175098c64d695b2454766cb618
chenk keinan <chen.keinan@appsflyer.com>
2020-11-04 22:37:24 +0200

    Add permission to docker build file

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -5,7 +5,7 @@ sudo apt-get install docker-ce
 # build docker image
 docker build ./ -t kube-beacon -f Dockerfile
 # login to registry
-docker login beacon.jfrog.io -u $(USER) -p $(PASSWORD)
+docker login beacon.jfrog.io -u $USER -p $PASSWORD
 # tag image image
 docker tag kube-beacon:latest beacon.jfrog.io/docker-local/kube-beacon:latest
 # push image to repository
