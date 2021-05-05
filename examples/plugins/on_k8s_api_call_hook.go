15c83f9d6afdab76dd2451672a1b9269eaea24ae
chen keinan <hen.keinan@gmail.com>
2021-05-05 23:43:03 +0300

    update plugin example

--- examples/plugins/on_k8s_api_call_hook.go
+++ examples/plugins/on_k8s_api_call_hook.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/chen-keinan/beacon/pkg/models"
+	"net/http"
+	"strings"
 )
 
 //K8sBenchAuditResultHook this plugin method accept k8s audit bench results
@@ -13,6 +15,15 @@ func K8sBenchAuditResultHook(k8sAuditResults models.KubeAuditResults) error {
 	var sb = new(bytes.Buffer)
 	err := json.NewEncoder(sb).Encode(k8sAuditResults)
 	fmt.Print(k8sAuditResults)
+	if err != nil {
+		return err
+	}
+	req, err := http.NewRequest("POST", "http://localhost:8090/audir-results", strings.NewReader(sb.String()))
+	if err != nil {
+		return err
+	}
+	client := http.Client{}
+	_, err = client.Do(req)
 	if err != nil {
 		return err
 	}
